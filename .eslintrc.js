6a8677e55e68cef9a27c611587f134c6a807be25
jindyu <ihris219@gamil.com>
2021-11-28 23:59:00 +0900

    websocket chat

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
   extends: ['airbnb-base', 'eslint-', 'prettier'],
+  rules: {
+    'import/prefer-default-export': ['off'],
+  },
 }
--- src/main.js
+++ src/main.js
@@ -2,16 +2,40 @@
 const Koa = require('koa')
 const path = require('path')
 const Pug = require('koa-pug')
+const route = require('koa-route')
+const websockify = require('koa-websocket')
+const serve = require('koa-static')
+const mount = require('koa-mount')
 
-const app = new Koa()
+const app = websockify(new Koa())
 
 new Pug({
   viewPath: path.resolve(__dirname, './views'),
   app,
 })
 
+app.use(mount('/public', serve('src/public')))
+
 app.use(async (ctx) => {
   await ctx.render('main')
 })
+
+// Using routes
+app.ws.use(
+  route.all('/ws', (ctx) => {
+    ctx.websocket.on('message', (data) => {
+      if (typeof data !== 'string') return
+
+      const { message, nickname } = JSON.parse(data)
+
+      const { server } = app.ws
+      if (!server) return
+
+      server.clients.forEach((client) => {
+        client.send(JSON.stringify({ message, nickname }))
+      })
+    })
+  })
+)
 
 app.listen(5000)
